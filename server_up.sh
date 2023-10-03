aa6da09477163cb2cedfda622eae2b4ac6a00842
suraj <surajkadam50@gmail.com>
2023-10-04 04:07:43 +0530

    first

---
+++ server_up.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+backend="https://github.com/ignitingMinds/quickquotebackend.git"
+frontend="https://github.com/ignitingMinds/quickquoteUI.git"
+
+directory_name="bharat-crm"
+
+if [ -d "$directory_name" ]; then
+    rm -rf "$directory_name"
+    echo "Directory $directory_name has been removed."
+else
+    echo "Directory $directory_name does not exist."
+fi
+
+echo "Creating directory $directory_name"
+mkdir $directory_name
+cd $directory_name
+pwd
+
+echo "cloning backend"
+git clone $backend
+
+echo "cloning frontend"
+git clone $frontend
+
+# echo "cloning docker compose file"
+# git clone $docker_compose
+# mv bharat-crm-compose/docker-compose.yml
+# rm -rf bharat-crm-compose
+cp ../docker-compose.yml .
+
+# loading the configurations 
+echo "adding configurations"
+cp ../configs/config.yaml quickquotebackend/src/config/
+cp ../configs/.env quickquoteUI/
+
+
+
+# code related to the docker compoes up
+# creating image and deploying
+echo "running docker-compose"
+docker compose up -d 
+
