e90a133a191339602ea2665c762b01d5622068d8
suraj <surajkadam50@gmail.com>
2023-10-22 04:26:11 +0530

    conditional start

--- server_up.sh
+++ server_up.sh
@@ -3,53 +3,48 @@ hardStart=$1
 
 backend="git@github.com:ignitingMinds/bcrmBackend.git"
 frontend="git@github.com:ignitingMinds/quickquoteUI.git"
-
 directory_name="bharat-crm"
-
-if [ -d "$directory_name" ]; then
-    rm -rf "$directory_name"
-    echo "Directory $directory_name has been removed."
-else
-    echo "Directory $directory_name does not exist."
-fi
-
-echo "Creating directory $directory_name"
-mkdir $directory_name
-#sudo chown -R $USER ./$directory_name
-cd $directory_name
-pwd
-
-echo "cloning backend"
-git clone $backend
-cd bcrmBackend
-git checkout QA
-cd ..
-
-
-echo "cloning frontend"
-git clone $frontend
-cd quickquoteUI
-git checkout UAT
-cd ..
-
-# echo "cloning docker compose file"
-# git clone $docker_compose
-# mv bharat-crm-compose/docker-compose.yml
-# rm -rf bharat-crm-compose
-cp ../docker-compose.yml .
-
-# loading the configurations 
-echo "adding configurations"
-cp ../configs/config.yaml bcrmBackend/src/config/
-cp ../configs/.env quickquoteUI/
-
-# down
-echo "down previous compose"
-sudo docker-compose down
-
 
 if [ "$hardStart" = "true" ]; then
     echo "........HARD START........."
+        
+    if [ -d "$directory_name" ]; then
+        rm -rf "$directory_name"
+        echo "Directory $directory_name has been removed."
+    else
+        echo "Directory $directory_name does not exist."
+    fi
+
+    echo "Creating directory $directory_name"
+    mkdir $directory_name
+    cd $directory_name
+    pwd
+
+    echo "cloning backend"
+    git clone $backend
+    cd bcrmBackend
+    git checkout QA
+    cd ..
+
+
+    echo "cloning frontend"
+    git clone $frontend
+    cd quickquoteUI
+    git checkout UAT
+    cd ..
+
+    # copying yaml file
+    cp ../docker-compose.yml .
+
+    # loading the configurations 
+    echo "adding configurations"
+    cp ../configs/config.yaml bcrmBackend/src/config/
+    cp ../configs/.env quickquoteUI/
+
+    # down
+    echo "down previous compose"
+    sudo docker-compose down
+
     echo "removing old images"
     sudo docker rmi bharat-crm_crm-backend
     sudo docker rmi bharat-crm_crm-frontend
