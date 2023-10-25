88d3e995a1b9e1169f8ea27b33946dbbcd3b77d3
suraj <surajkadam50@gmail.com>
2023-10-25 21:55:37 +0530

    refactored server_up.sh

--- server_up.sh
+++ server_up.sh
@@ -1,59 +1,93 @@
 #!/bin/bash
 hardStart=$1
+service=$2
+
+echo "hardStart : " $hardStart
+echo "service   : "$service
 
 backend="git@github.com:ignitingMinds/bcrmBackend.git"
 frontend="git@github.com:ignitingMinds/quickquoteUI.git"
 directory_name="bharat-crm"
 
+flag="true"
+
+# --------------------------------------------------------------------
 if [ "$hardStart" = "true" ]; then
     echo "........HARD START........."
-        
-    if [ -d "$directory_name" ]; then
-        rm -rf "$directory_name"
-        echo "Directory $directory_name has been removed."
-    else
-        echo "Directory $directory_name does not exist."
-    fi
-
-    echo "Creating directory $directory_name"
-    mkdir $directory_name
-    cd $directory_name
-    pwd
-
-    echo "cloning backend"
-    git clone $backend
-    cd bcrmBackend
-    git checkout QA
-    cd ..
-
-
-    echo "cloning frontend"
-    git clone $frontend
-    cd quickquoteUI
-    git checkout UAT
-    cd ..
-
-    # copying yaml file
-    cp ../docker-compose.yml .
-
-    # loading the configurations 
-    echo "adding configurations"
-    cp ../../configs/config.yaml bcrmBackend/src/config/
-    cp ../../configs/.env quickquoteUI/
-
-    # down
-    echo "down previous compose"
-    sudo docker-compose down
-
-    echo "removing old images"
-    sudo docker rmi bharat-crm_crm-backend
-    sudo docker rmi bharat-crm_crm-frontend
 else
     cd $directory_name
     echo ".......SOFT START.........."
 fi
+# --------------------------------------------------------------------
 
-# code related to the docker compoes up
+
+# --------------------------------------------------------------------
+if [ -d "$directory_name" ]; then
+    flag="false"    
+else
+    echo "Creating directory $directory_name"
+    mkdir $directory_name
+fi
+# --------------------------------------------------------------------
+
+# --------------------------------------------------------------------
+cd $directory_name
+echo copying docker compose file
+cp ../docker-compose.yml .
+
+# down
+echo "down previous compose"
+#sudo docker-compose down
+# --------------------------------------------------------------------
+
+
+# ---------------------------BACKEND------------------------------------
+if [ "$hardStart" = "true" ] && { [ "$service" -eq 3 ] || [ "$service" -eq 1 ]; }; then 
+    if [ "$flag" = "true" ]; then
+        echo "cloning backend"
+        git clone $backend
+        cd bcrmBackend
+        git checkout QA
+        cd ..
+    else
+        echo "running git pull"
+        cd $backend
+        git pull 
+        cd ..
+    fi
+
+    echo loading the backend configurations 
+    cp ../../configs/config.yaml bcrmBackend/src/config/
+  
+    echo "removing old backend images"
+    #sudo docker rmi bharat-crm_crm-backend
+fi
+# --------------------------------------------------------------------
+
+# ---------------------------FRONTEND---------------------------------
+if [ "$hardStart" = "true" ] && { [ "$service" -eq 3 ] || [ "$service" -eq 2 ]; }; then 
+    if [ "$flag" = "true" ]; then
+        echo "cloning frontend"
+        git clone $frontend
+        cd quickquoteUI
+        git checkout UAT
+        cd ..
+    else
+        echo "running git pull"
+        cd $frontend
+        git pull 
+        cd ..
+    fi
+
+    # loading the configurations 
+    echo loading the frontend configurations 
+    cp ../../configs/.env quickquoteUI/
+
+    echo "removing old frontend images"
+    #sudo docker rmi bharat-crm_crm-frontend
+fi
+# --------------------------------------------------------------------
+
 # creating image and deploying
-echo "running docker-compose"
-sudo docker-compose up -d 
+echo "running docker-compose up"
+#sudo docker-compose up -d
